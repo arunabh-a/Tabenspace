d1fc2fbb3397408a5cc41271168b2136f12f589c
Arunabh <arunabh.nd@gmail.com>
2023-04-04 20:42:26 +0530

    Enter Search Issue Resolved

--- scripts/Search.js
+++ scripts/Search.js
@@ -1,7 +1,16 @@
-function Search() {
-  var text = document.getElementById("search").value;
-  var cleanQuery = text.replace(" ", "+", text);
-  var url = 'http://www.google.com/search?q=' + cleanQuery;
+// function Search() {
+//   var text = document.getElementById("search").value;
+//   var cleanQuery = text.replace(" ", "+", text);
+//   var url = 'http://www.google.com/search?q=' + cleanQuery;
 
-  window.location.href = url;
-}
+//   window.location.href = url;
+// }
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (event) => {
+event.preventDefault();
+const query = form.elements.querysearch.value; 
+const url = `https://www.google.com/search?querysearch=${query}`;
+window.open(url, '_blank');
+});
